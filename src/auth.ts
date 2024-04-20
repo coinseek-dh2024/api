814afeca49e42380fda8b73355831612e8bf0742
Gašper Dobrovoljc <gasper.dobrovoljc1@gmail.com>
2024-04-20 19:17:34 +0200

    Update user info

--- src/auth.ts
+++ src/auth.ts
@@ -1,19 +1,31 @@
 import admin from 'firebase-admin';
 import { Request, Response, NextFunction } from 'express';
+import { prisma } from './prisma';
 
 export async function auth(req: Request, res: Response, next: NextFunction) {
   const authHeader = req.header('Authorization');
   if (!authHeader) {
-    return res.sendStatus(401);
+    return res
+      .status(401)
+      .json({ message: 'Token missing from authorization header' });
   }
   const token = authHeader.split('Bearer ')[1];
 
   const decToken = await admin.auth().verifyIdToken(token);
   if (!decToken) {
-    return res.sendStatus(401);
+    return res.status(401).json({ message: 'Invalid token' });
   }
 
   req.userId = decToken.uid;
+
+  const user = await prisma.user.findUnique({
+    where: { id: decToken.uid },
+  });
+  if (!user) {
+    await prisma.user.create({
+      data: { id: decToken.uid },
+    });
+  }
 
   next();
 }
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,28 @@
-import express from 'express';
-import { auth } from './auth.js';
+import express, { Request, Response } from 'express';
+import { auth } from './auth';
+import { prisma } from './prisma';
 
 const app = express();
 const port = 3000;
 
-// app.use(auth);
+app.use(auth);
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('healthy');
 });
 
-app.patch('/user', (req, res) => {
-  res.status(200).end();
+app.patch('/user', async (req: Request, res: Response) => {
+  const user = await prisma.user.update({
+    where: { id: req.userId },
+    data: {
+      username: req.body.username,
+      name: req.body.user,
+    },
+    select: { username: true, name: true },
+  });
+
+  res.json(user);
 });
 
 app.listen(port, () => {
